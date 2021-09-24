291724fb2cca420317a90b1ec8053395f7228820
kiran <kiranreddy@kiran-Reddy-2.local>
2021-09-24 12:38:48 -0400

    initial code commit with basic skeleton ui

---
+++ Assignment/src/model/Product.java
@@ -0,0 +1,150 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author kiranreddy
+ */
+public class Product {
+    private String name;
+    private String geodata;
+    private String dob;
+    private String telenum;
+    private String faxnum;
+    private String email;
+    private String ssn;
+    private String medrec;
+    private String Hpbnum;
+    private String banum;
+    private String licnum;
+    private String sernum;
+    private String devnum;
+    private String linkdin;
+    private String ipaddr;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGeodata() {
+        return geodata;
+    }
+
+    public void setGeodata(String geodata) {
+        this.geodata = geodata;
+    }
+
+    public String getDob() {
+        return dob;
+    }
+
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
+
+    public String getTelenum() {
+        return telenum;
+    }
+
+    public void setTelenum(String telenum) {
+        this.telenum = telenum;
+    }
+
+    public String getFaxnum() {
+        return faxnum;
+    }
+
+    public void setFaxnum(String faxnum) {
+        this.faxnum = faxnum;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSsn() {
+        return ssn;
+    }
+
+    public void setSsn(String ssn) {
+        this.ssn = ssn;
+    }
+
+    public String getMedrec() {
+        return medrec;
+    }
+
+    public void setMedrec(String medrec) {
+        this.medrec = medrec;
+    }
+
+    public String getHpbnum() {
+        return Hpbnum;
+    }
+
+    public void setHpbnum(String Hpbnum) {
+        this.Hpbnum = Hpbnum;
+    }
+
+    public String getBanum() {
+        return banum;
+    }
+
+    public void setBanum(String banum) {
+        this.banum = banum;
+    }
+
+    public String getLicnum() {
+        return licnum;
+    }
+
+    public void setLicnum(String licnum) {
+        this.licnum = licnum;
+    }
+
+    public String getSernum() {
+        return sernum;
+    }
+
+    public void setSernum(String sernum) {
+        this.sernum = sernum;
+    }
+
+    public String getDevnum() {
+        return devnum;
+    }
+
+    public void setDevnum(String devnum) {
+        this.devnum = devnum;
+    }
+
+    public String getLinkdin() {
+        return linkdin;
+    }
+
+    public void setLinkdin(String linkdin) {
+        this.linkdin = linkdin;
+    }
+
+    public String getIpaddr() {
+        return ipaddr;
+    }
+
+    public void setIpaddr(String ipaddr) {
+        this.ipaddr = ipaddr;
+    }
+      
+    
+}
